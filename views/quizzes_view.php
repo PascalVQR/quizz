ff1a873db4e61853c5d5530230a6505c97cb206a
PascalVQR <168442344+PascalVQR@users.noreply.github.com>
2024-11-15 14:15:42 +0100

    Ajout page "Tous les Quiz", modifs CSS (#7)
    * header WIP
    
    * Ajout header & .htaccess
    
    * Header + .htaccess
    
    * Update header_view.php
    
    * Update header_view.php
    
    * Header + .htaccess
    
    * Header + .htaccess
    
    * css actualisé
    
    * Quiz Grid WIP
    
    * Grille tous les quiz

---
+++ views/quizzes_view.php
@@ -0,0 +1,27 @@
+    <main>
+        <section class="search-menu">
+            <section class="search-header">
+                <h2>Recherche et Filtres</h2>
+                <span class="material-symbols-outlined">keyboard_arrow_down</span>
+            </section>
+        </section>
+        <h2>Tous les Quiz</h2>
+        <section class="quiz-grid">
+            <article class="quiz-card">
+            <img src="assets/img/linked_in_logo.png" alt="">
+                <p>PHP</p>
+            </article>
+            <article class="quiz-card">
+                <img src="assets/img/linked_in_logo.png" alt="">
+                <p>PHP</p>
+            </article>
+            <article class="quiz-card">
+            <img src="assets/img/linked_in_logo.png" alt="">
+                <p>PHP</p>
+            </article>
+            <article class="quiz-card">
+                <img src="assets/img/linked_in_logo.png" alt="">
+                <p>PHP</p>
+            </article>
+        </section>
+    </main>
