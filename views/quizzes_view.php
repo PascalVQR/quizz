a9df2d44283580263050857282421912d9b0e1bd
PascalVQR <pascal.vaquer@outlook.fr>
2024-11-15 14:08:46 +0100

    Grille tous les quiz

--- views/quizzes_view.php
+++ views/quizzes_view.php
@@ -8,7 +8,20 @@
         <h2>Tous les Quiz</h2>
         <section class="quiz-grid">
             <article class="quiz-card">
-
+            <img src="assets/img/linked_in_logo.png" alt="">
+                <p>PHP</p>
+            </article>
+            <article class="quiz-card">
+                <img src="assets/img/linked_in_logo.png" alt="">
+                <p>PHP</p>
+            </article>
+            <article class="quiz-card">
+            <img src="assets/img/linked_in_logo.png" alt="">
+                <p>PHP</p>
+            </article>
+            <article class="quiz-card">
+                <img src="assets/img/linked_in_logo.png" alt="">
+                <p>PHP</p>
             </article>
         </section>
     </main>
